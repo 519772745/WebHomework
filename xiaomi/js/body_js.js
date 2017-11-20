742d7a9d90ae9268b5df053c7be48a506c37bf45
孙亦璇 <519772745@qq.com>
2017-11-20 11:41:01 +0800

    xiaomi
    _(:з」∠)_

---
+++ xiaomi/js/body_js.js
@@ -0,0 +1,15 @@
+var lists=document.getElementById('show');
+console.log(lists);
+/*lists.onmouseover=function(){
+	lists.style.color='#ff8100';
+		lists.style.paddingBottom='5px';
+		lists.style.borderBottom='2px solid #ff8100';
+}*/
+//for (var i = 0; i < lists.length; i++) {
+	//console.log("1");
+	/*lists[i].onmouseover=function(){
+		lists[i].style.color='#ff8100';
+		lists[i].style.paddingBottom='5px';
+		lists[i].style.borderBottom='2px solid #ff8100';
+	}*/
+//};
